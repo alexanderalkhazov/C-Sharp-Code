11ec1b631c9adcca8b693cc9c26e4522b8439e03
Alexander A <126073265+alexanderalkhazov@users.noreply.github.com>
2023-05-24 11:06:37 +0300

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+	internal class Program
+	{
+		static TableServers host1List = TableServers.GetTableServers();
+		static TableServers host2List = TableServers.GetTableServers();
+
+		static void Main(string[] args)
+		{
+			TableServers tableServers = TableServers.GetTableServers();
+			for (int i = 0; i < 5; i++)
+			{
+				HostOneGetNextServer();
+				HostTwoGetNextServer();
+			}
+			Console.ReadLine();
+		}
+		private static void HostOneGetNextServer()
+		{
+			Console.WriteLine("The Server Is   :  " + host1List.GetNextServer());
+		}
+		private static void HostTwoGetNextServer()
+		{
+			Console.WriteLine("The Server Is   :  " + host2List.GetNextServer());
+		}
+	}
+}
---
+++ Singleton/TableServers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+	public class TableServers
+	{
+		private static readonly TableServers _instance = new TableServers();
+		private List<string> _servers= new List<string>();
+		private int nextServer = 0;
+
+		private TableServers()
+		{
+			_servers.Add("Tim");
+			_servers.Add("Sue");
+			_servers.Add("Marry");
+			_servers.Add("Bob");
+		}
+
+		public static TableServers GetTableServers()
+		{
+			return _instance;
+		}
+
+		public string GetNextServer()
+		{
+			if (nextServer >= _servers.Count)
+			{
+				nextServer = 0;
+			}
+			var res = _servers[nextServer];
+			nextServer++;
+			return res;		
+		}
+	}
+}
